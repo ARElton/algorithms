1451cb1137db8a365eb4d261c99eda46a7487c6f
ARElton <ameliareneeelton@gmail.com>
2021-05-09 21:35:19 -0400

    update anagrams

--- anagrams.js
+++ anagrams.js
@@ -1,18 +1,45 @@
 // --- Directions
-// Write a function that accepts a string.  The function should
-// capitalize the first letter of each word in the string then
-// return the capitalized string.
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
 // --- Examples
-//   capitalize('a short sentence') --> 'A Short Sentence'
-//   capitalize('a lazy fox') --> 'A Lazy Fox'
-//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
 
-function capitalize(str) {
-    const words = []
-
-    for (let word of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1))
-    }
-
-    return words.join(' ')
+function anagrams(stringA, stringB) {
+    return cleanString(stringA) === cleanString(stringB)
 }
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('')
+}
+
+
+// function anagrams(stringA, stringB) {
+//     const aCharMap = buildCharMap(stringA)
+//     const bCharMap = buildCharMap(stringB)
+
+//     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//         return false 
+//     }
+
+//     for (let char in aCharMap) {
+//         if (aCharMap[char] !== bCharMap[char]) {
+//             return false
+//         }
+//     }
+
+//     return true
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {}
+
+//     for (let char of str.replace(/[^\w]/g , "").toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1
+//     }
+
+//     return charMap
+// }
